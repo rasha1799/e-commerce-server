f0e5342398800fb772ae92f2f46f3783eb22c642
rashed1799 <abuhasnat22@student.sust.edu>
2022-01-04 07:28:47 +0600

    server

--- index.js
+++ index.js
@@ -22,12 +22,65 @@ async function run() {
     await client.connect();
     const database = client.db("e-commerce12");
     const productCollection = database.collection("productsData");
-
+    const orderCollection = database.collection("ordersData");
     //GET Products API
     app.get("/products", async (req, res) => {
       const cursor = productCollection.find({});
       const products = await cursor.toArray();
       res.send(products);
+    });
+    // GET Single product
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("getting specific product", id);
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.json(product);
+    });
+    // Use POST to get data by keys
+    app.post("/products/byKeys", async (req, res) => {
+      const keys = req.body;
+      const query = { key: { $in: keys } };
+      const products = await productCollection.find(query).toArray();
+      res.send(products);
+    });
+    // Add Orders API
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.json(result);
+    });
+
+    //GET orders API
+    app.get("/orders", async (req, res) => {
+      const cursor = orderCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    //get my Orders
+    app.get("/myOrders/:email", async (req, res) => {
+      const result = await orderCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
+
+    //post API
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      console.log(product);
+      const result = await productCollection.insertOne(product);
+      res.json(result);
+    });
+
+    //Delete api
+    app.delete("/deleteProducts/:id", async (req, res) => {
+      const result = await productCollection.deleteOne({
+        _id: req.params.id,
+      });
+      console.log(result);
+      res.send(result);
     });
   } finally {
     // await client.close();
